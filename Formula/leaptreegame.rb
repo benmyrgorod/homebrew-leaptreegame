f5eb0bce598adbe0e664f390c4ed1288810470f0
Ben Myrgorod <65790228+benmyrgorod@users.noreply.github.com>
2026-05-24 00:24:44 -0400

    Add formula

---
+++ Formula/leaptreegame.rb
@@ -0,0 +1,18 @@
+class Leaptreegame < Formula
+  include Language::Python::Virtualenv
+
+  desc "Leap Tree Game is an experimental AI storytelling game"
+  homepage "https://github.com/benmyrgorod/homebrew-leaptreegame"
+  url "https://github.com/benmyrgorod/homebrew-leaptreegame/releases/download/v0.3.1/leaptreegame-0.3.1.tar.gz"
+  sha256 "c21646b7680bbaa4810731cf0349118c2a284e26997fb06731fbce7ab49253f2"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    venv = virtualenv_create(libexec, "python3.12")
+    system Formula["python@3.12"].opt_bin/"python3.12", "-m", "pip", "--python=#{venv.root}/bin/python", "install", "--no-cache-dir", "."
+
+    bin.install_symlink (libexec/"bin/leaptreegame")
+  end
+end
